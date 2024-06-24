983297d0c68f4fed8cf2048ffd8caa732c60b588
JeffersonJesusDev <jefferson3680@hotmail.com>
2024-06-24 15:16:26 -0300

    Usando o Chat GPT para traduzir a sinopse

--- src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -5,7 +5,7 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
     public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService("colar API");
+        OpenAiService service = new OpenAiService("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");
 
         CompletionRequest requisicao = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
